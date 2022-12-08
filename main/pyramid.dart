d3354bcd0e26d3397a48da331ca54ad76bfe97e3
Hans Figo <hambasahaya0303@gmail.com>
2022-12-08 17:14:41 +0700

    add_class

--- main/pyramid.dart
+++ main/pyramid.dart
@@ -1,50 +1,12 @@
 import 'dart:io';
 
 void main() {
+  int y = 5;
 
-  // int y = 2;
-
-  for (var i = 0; i < 2; i++) { //1
-    for (var x = 0; x < 2; x++) {//
+  for (var i = 0; i < y; i++) {
+    for (var j = 0; j <= i; j++) {
+      stdout.write('*');
     }
-    print('\n');
+    print('');
   }
-
-  // if(0 < 0){
-  //   print('object');
-  // }else{
-  //   print('p');
-  // }
-
-  //Masuk Foor Loop
-  // for (int i = 1; i <= rows; i++) { //Inisialisasi berapa Kali Looping
-  //   for (int j = rows-i; j > 0; j--) { //jika j
-  //     stdout.write(' ');
-  //   }
-  //   for (int y = 1; y <= i; y++) { //jika j
-  //     stdout.write('*');
-  //   }
-  //   print('\n');
-  // }
 }
-
-// i = 0
-// 0<=3
-// print i(0)
-// i ++
-
-// i=1
-// 1<=3
-// print 1
-// i++
-
-// i=2
-// 2<+3
-// print 2
-// i++
-
-// i=3
-// 3<=3
-// print 3
-// i++
-//
